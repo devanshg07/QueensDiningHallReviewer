514875aca3b391684e5072d02c608fc037ee8134
Devansh Goyal <devanshgoyal087@gmail.com>
2025-09-17 03:55:23 -0400

    remove unneccessary files

--- downloadchrome.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-
-# Automatically finds the right ChromeDriver
-driver = webdriver.Chrome(options=options)
